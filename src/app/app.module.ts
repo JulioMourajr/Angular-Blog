0a6852ab289c71c17d8b3134b354893de1aa0236
Julio Moura <juliomourajr@hotmail.com>
2022-11-10 01:55:44 -0300

    Atualizando o projeto, criação dos componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { TituloComponent } from './componentes/titulo/titulo.component';
+import { BigCardComponent } from './componentes/big-card/big-card.component';
+import { SmallCardComponent } from './componentes/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TituloComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
