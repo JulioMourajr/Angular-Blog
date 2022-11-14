f5b43b274b4708b6f5f189fa5255157c021ca0b1
Julio Moura <juliomourajr@hotmail.com>
2022-11-13 23:34:09 -0300

    Adicionando multiplos css

--- src/app/componentes/titulo/titulo.component.ts
+++ src/app/componentes/titulo/titulo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.css', './titulo.responsive.component.css']
 })
 export class TituloComponent implements OnInit {
 
